830d6797efe0fe166f74f754f3f147e2db7792be
jinxiang <2452393862@qq.com>
2021-03-14 16:43:17 +0800

    commit

--- doctor_offline/review_model/__init__.py
+++ doctor_offline/review_model/__init__.py
@@ -3,3 +3,6 @@
 # @Time    : 2021/3/14 16:19
 # @Author  : JJkinging
 # @File    : __init__.py.py
+
+
+
--- doctor_offline/review_model/train.py
+++ doctor_offline/review_model/train.py
@@ -3,3 +3,16 @@
 # @Time    : 2021/3/14 16:23
 # @Author  : JJkinging
 # @File    : train.py
+import pandas as pd
+from collections import Counter
+
+# 读取数据
+train_data_path = './train_data.csv'
+train_data = pd.read_csv(train_data_path, header=None, sep='\t')
+
+# 打印正负标签比例
+print(dict(Counter(train_data[0].values)))
+
+# 转换数据到列表形式
+train_data = train_data.values[0].tolist()
+print(train_data[:10])
